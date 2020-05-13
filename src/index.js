318b4e24ef8378648bc6e7bf759d36ad278c966f
Shawn Mahon <shawn.mahon@kuka.com>
2020-05-13 13:55:23 -0500

    Section 2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
 //Create a react component
 const App = () => {
-  return <div> Hi there!</div>;
+  const buttonText = { text: 'Click me!'}
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter Name:</label>
+      <input id="name" type="text" />
+      <button style= {{backgroundColor: 'blue', color: 'white'}}> 
+        {buttonText.text} 
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
